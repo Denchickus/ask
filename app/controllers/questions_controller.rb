983a78e1eacb4d4a300569e6e76bdb6c228ff322
Denis Tarasov <denchickuz@yandex.ru>
2023-07-16 03:23:43 +0300

    add redirect to questions path after create question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,10 +2,12 @@ class QuestionsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    Question.create(
+    question = Question.create(
       body: params[:question][:body],
       user_id: params[:question][:user_id]
       )
+
+    redirect_to question_path(question)
   end
 
   def update
