7e0554a6b32a887df74e417261a7ea0246fb60e2
Denis Tarasov <denchickuz@yandex.ru>
2023-07-16 19:47:56 +0300

    Add question edit page

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -15,6 +15,8 @@ class QuestionsController < ApplicationController
       body: params[:question][:body],
       user_id: params[:question][:user_id]
       )
+
+    redirect_to question_path(@question)
   end
 
   def destroy
@@ -33,6 +35,10 @@ class QuestionsController < ApplicationController
 
   def new
     @question = Question.new
+  end
+
+  def edit
+    @question = Question.find(params[:id])
   end
 
 end
